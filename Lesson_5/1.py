d43090c7a0dd27ebcb9deb3729c9a9d2f8e20a4a
Zinfir <zinfirmail@gmail.com>
2019-04-11 15:24:23 +0300

    Home work 5

--- Lesson_5/1.py
+++ Lesson_5/1.py
@@ -5,3 +5,81 @@
 вывести наименования предприятий, чья прибыль выше среднего и отдельно
 вывести наименования предприятий, чья прибыль ниже среднего.
 """
+
+"""
+Реализация через колекции, collections.namedtuple
+"""
+
+import collections
+
+n = int(input('Введите количество предприятий: '))
+
+Companies = []
+
+for company in range(n):
+    name = input('Введите имя {}-й компании: '.format(company + 1))
+    Company = collections.namedtuple(name, ['q1', 'q2', 'q3', 'q4', 'year_earn'])
+    q1 = float(input('Введите прибыль за {} квартал: '.format(1)))
+    q2 = float(input('Введите прибыль за {} квартал: '.format(2)))
+    q3 = float(input('Введите прибыль за {} квартал: '.format(3)))
+    q4 = float(input('Введите прибыль за {} квартал: '.format(4)))
+    year_earn = q1 + q2 + q3 + q4
+    c = Company(q1=q1, q2=q2, q3=q3, q4=q4, year_earn=year_earn)
+    Companies.append(c)
+
+print('Список предприятий:')
+for company in Companies:
+    print(company)
+
+sum = 0
+for company in Companies:
+    sum += company.year_earn
+avg = sum / n
+
+print('Средня прибыль предприятий: {}'.format(avg))
+print('Предприятия с прибылью выше среднего: ')
+for company in Companies:
+    if company.year_earn > avg:
+        print(company)
+print('Предприятия с прибылью ниже среднего: ')
+for company in Companies:
+    if company.year_earn < avg:
+        print(company)
+
+
+"""
+Реализация без коллекций
+"""
+
+# n = int(input('Введите количество предприятий: '))
+
+# Companies = {}
+
+# for company in range(n):
+#     name = input('Введите имя {}-й компании: '.format(company + 1))
+#     Companies[name] = []
+#     year_earn = 0
+#     for q in range(4):
+#         q_earn = float(input('Введите прибыль за {} квартал: '.format(q + 1)))
+#         Companies[name].append(q_earn)
+#         year_earn += q_earn
+#     Companies[name].append(year_earn)
+
+# for key, value in Companies.items():
+#     print('Предприятие - {}, прибыль по кварталам - {}, годовая прибыль - {}'.format(key, value[0:4], value[4]))
+
+
+# sum = 0
+# for key, value in Companies.items():
+#     sum += value[4]
+# avg = sum / n
+
+# print('Средня прибыль предприятий: {}'.format(avg))
+# print('Предприятия с прибылью выше среднего: ')
+# for key, value in Companies.items():
+#     if value[4] > avg:
+#         print('{} - {}'.format(key, value[4]))
+# print('Предприятия с прибылью ниже среднего: ')
+# for key, value in Companies.items():
+#     if value[4] < avg:
+#         print('{} - {}'.format(key, value[4]))
--- Lesson_5/2.py
+++ Lesson_5/2.py
@@ -5,3 +5,47 @@
 [‘C’, ‘4’, ‘F’] соответственно. Сумма чисел из примера: [‘C’, ‘F’, ‘1’],
 произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
 """
+
+import collections
+
+defdict = collections.defaultdict(list)
+
+a = 'A2'
+A = [_ for _ in a]
+
+b = 'C4F'
+B = [_ for _ in b]
+
+def_dict = collections.defaultdict(list)
+
+def_dict[a] = A
+def_dict[b] = B
+
+print(def_dict)
+value_A = 0
+for index, value in enumerate(def_dict[a][::-1]):
+    value_A += int(value, 16) * 16**index
+
+value_B = 0
+for index, value in enumerate(def_dict[b][::-1]):
+    value_B += int(value, 16) * 16**index
+
+sum = value_A + value_B
+mul = value_A * value_B
+
+sum_hex = hex(sum)[2:]
+mul_hex = hex(mul)[2:]
+SUM = [_ for _ in sum_hex]
+MUL = [_ for _ in mul_hex]
+def_dict[sum_hex] = SUM
+def_dict[mul_hex] = MUL
+
+for index, item in enumerate(def_dict):
+    if index == 0:
+        print('Первое число: ', item)
+    elif index == 1:
+        print('Второе число: ', item)
+    elif index == 2:
+        print('Сумма:        ', item)
+    elif index == 3:
+        print('Умножение:    ', item)
